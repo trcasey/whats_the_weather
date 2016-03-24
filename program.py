3b5f3d8f7d04a443eb53b64f16996db454b8dcfc
Trent Casey <trcasey@outlook.com>
2016-03-23 20:49:22 -0400

    added functions to retrieve weather data.

--- program.py
+++ program.py
@@ -1,13 +1,17 @@
+import requests
+import bs4
 
 def main():
     print_the_header()
 
     code = input('What zipcode do you want the weather for (10023)? ')
-    #get html from web
-    print(code)
+
+    html = get_html_from_web(code)
+
     #parse teh html
+    get_weather_from_html(html)
+
     #display for the forecast
-    print('hello from main')
 
 
 def print_the_header():
@@ -17,6 +21,22 @@ def print_the_header():
     print()
 
 
+def get_html_from_web(zipcode):
+    url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
+    #print(url)
+    response = requests.get(url)
+    # print(response.status_code)
+    # print(response.text[0:250])
+
+    return response.text
+
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    loc = soup.find(id='location').find('h1').get_text()
+    condition = soup.find(id='curCond').find(class_='wx-value').get_text()
+    temp = soup.find(id='curTemp').find(class_='wx-value').get_text()
+    scale = soup.find(id='curTemp').find(class_='wx-unit').get_text()
+    print(condition, temp, scale)
 
 if __name__ == '__main__':
     main()
